a5821deddf11a7c4044931faaf7bc63a1481b11e
premanshupandey <premanshuppandey@gmail.com>
2023-11-09 01:06:21 +0530

    Added: grunt release command to create ready to use plugin by removing development files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
 module.exports = function (grunt) {
+  var pkgInfo = grunt.file.readJSON("package.json");
+
   grunt.initConfig({
     cssmin: {
       build: {
@@ -26,13 +28,66 @@ module.exports = function (grunt) {
         },
       },
     },
+
+    clean: {
+      main: ["exit-wizard"],
+      zip: ["*.zip"],
+    },
+
+    copy: {
+      main: {
+        options: {
+          mode: true,
+        },
+        src: [
+          "**",
+          "!node_modules/**",
+          "!.git/**",
+          "!.github/**",
+          "!bin/**",
+          "!Gruntfile.js",
+          "!package.json",
+          "!.gitignore",
+          "!README.md",
+          "!vendor/**",
+          "!composer.json",
+          "!composer.lock",
+          "!package-lock.json",
+          "!phpcs.xml.dist",
+        ],
+        dest: "exit-wizard/",
+      },
+    },
+
+    compress: {
+      main: {
+        options: {
+          archive: "exit-wizard" + pkgInfo.version + ".zip",
+          mode: "zip",
+        },
+        files: [
+          {
+            src: ["./exit-wizard/**"],
+          },
+        ],
+      },
+    },
   });
 
   // Minify task
   grunt.registerTask("minify", ["cssmin", "terser"]);
   // Generate POT file.
   grunt.loadNpmTasks("grunt-wp-i18n");
-
+  // Grunt release - Create installable package of the local files
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-compress");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.registerTask("release", [
+    "clean:zip",
+    "copy:main",
+    "compress:main",
+    "clean:main",
+  ]);
   // Load up tasks
   grunt.loadNpmTasks("grunt-terser");
   grunt.loadNpmTasks("grunt-contrib-cssmin");
